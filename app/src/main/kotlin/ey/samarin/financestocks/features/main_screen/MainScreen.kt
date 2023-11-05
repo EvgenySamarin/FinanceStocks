b157843f0722e99c87c828df5355c10927d6c89c
evgeny.samarin <evgeny.samarin@lenta.com>
2023-11-05 07:09:16 +0700

    fix_rapid_connection fix

--- app/src/main/kotlin/ey/samarin/financestocks/features/main_screen/MainScreen.kt
+++ app/src/main/kotlin/ey/samarin/financestocks/features/main_screen/MainScreen.kt
@@ -1,25 +1,36 @@
 package ey.samarin.financestocks.features.main_screen
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -28,26 +39,17 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import ey.samarin.financestocks.R
+import ey.samarin.financestocks.ui.theme.getAdaptiveBodyByHeight
+import ey.samarin.financestocks.ui.theme.getAdaptiveLabelByHeight
 import ey.samarin.financestocks.utils.PreviewUtils.WINDOWS_SIZE_COMPACT
 import ey.samarin.financestocks.utils.PreviewUtils.WINDOWS_SIZE_EXPANDED
 import ey.samarin.financestocks.utils.PreviewUtils.WINDOWS_SIZE_MEDIUM
+import ey.samarin.models.STUB_StockPreview
 import ey.samarin.models.StockPreview
 
 class MainUIStateProvider : PreviewParameterProvider<MainUIState> {
     override val values = sequenceOf(
-        MainUIState(
-            stocksPreview = listOf(
-                StockPreview(symbol = "AAPL"),
-                StockPreview(symbol = "TSLA"),
-                StockPreview(symbol = "GOOG"),
-                StockPreview(symbol = "AMZN"),
-                StockPreview(symbol = "MSFT"),
-                StockPreview(symbol = "FB"),
-                StockPreview(symbol = "NVDA"),
-                StockPreview(symbol = "PYPL"),
-                StockPreview(symbol = "INTC"),
-            )
-        )
+        MainUIState(stocksPreview = STUB_StockPreview)
     )
 }
 
@@ -55,7 +57,7 @@ class MainUIStateProvider : PreviewParameterProvider<MainUIState> {
 @Composable
 fun MainScreenTabletPreview() {
     MainScreen(
-        state = MainUIState(),
+        state = MainUIState(stocksPreview = STUB_StockPreview),
         windowSize = WINDOWS_SIZE_EXPANDED
     )
 }
@@ -64,7 +66,7 @@ fun MainScreenTabletPreview() {
 @Composable
 fun MainScreenPhonePreview() {
     MainScreen(
-        state = MainUIState(),
+        state = MainUIState(stocksPreview = STUB_StockPreview),
         windowSize = WINDOWS_SIZE_MEDIUM
     )
 }
@@ -73,7 +75,7 @@ fun MainScreenPhonePreview() {
 @Composable
 fun MainScreenWatchPreview() {
     MainScreen(
-        state = MainUIState(),
+        state = MainUIState(stocksPreview = STUB_StockPreview),
         windowSize = WINDOWS_SIZE_COMPACT
     )
 }
@@ -86,7 +88,7 @@ fun MainScreen(
     windowSize: WindowSizeClass = WINDOWS_SIZE_MEDIUM,
     onScreenLaunch: () -> Unit = {},
     onSearchTextChange: (String) -> Unit = {},
-    onRepeatClick: () -> Unit = {},
+    onStockPreviewTap: (StockPreview) -> Unit = {},
 ) {
     LaunchedEffect(Unit) {
         onScreenLaunch()
@@ -94,48 +96,55 @@ fun MainScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.main_screen_title)) },
-            )
+            if (windowSize.heightSizeClass != WindowHeightSizeClass.Compact) {
+                TopAppBar(
+                    title = { Text(text = stringResource(id = R.string.main_screen_title)) },
+                )
+            }
         },
         content = { innerPadding ->
-            Box(modifier = Modifier.padding(innerPadding)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+            ) {
                 when (windowSize.widthSizeClass) {
                     WindowWidthSizeClass.Compact,
-                    WindowWidthSizeClass.Medium -> Column(modifier = Modifier.padding(16.dp)) {
-                        TextField(
-                            value = state.searchText,
-                            onValueChange = { onSearchTextChange(it) },
+                    WindowWidthSizeClass.Medium -> Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxHeight()
+                    ) {
+                        SearchString(
+                            text = state.searchText,
+                            onTextChange = onSearchTextChange,
                             modifier = Modifier.fillMaxWidth(),
-                            colors = TextFieldDefaults.colors(
-                                unfocusedContainerColor = Color.Transparent,
-                                disabledContainerColor = Color.Transparent,
-                            ),
                         )
-                        Button(
-                            onClick = { onRepeatClick() },
-                            modifier = Modifier
-                                .padding(top = 8.dp)
-                                .fillMaxWidth(),
-                        ) {
-                            Text(text = "Repeat")
-                        }
-                        StocksListContent(state.stocksPreview)
+                        StocksPreviewListContent(
+                            stocksPreview = state.stocksPreview,
+                            windowSize = windowSize,
+                            modifier = Modifier.fillMaxWidth(),
+                            onItemTap = onStockPreviewTap,
+                        )
                     }
 
-                    WindowWidthSizeClass.Expanded -> Row {
-                        Column {
-                            TextField(
-                                value = state.searchText,
-                                onValueChange = { },
+                    WindowWidthSizeClass.Expanded -> Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Column(modifier = Modifier.weight(1f)) {
+                            SearchString(
+                                text = state.searchText,
+                                onTextChange = onSearchTextChange,
                                 modifier = Modifier.fillMaxWidth(),
-                                colors = TextFieldDefaults.colors(
-                                    unfocusedContainerColor = Color.Transparent,
-                                    disabledContainerColor = Color.Transparent,
-                                ),
                             )
                         }
-                        StocksListContent(state.stocksPreview)
+                        StocksPreviewListContent(
+                            stocksPreview = state.stocksPreview,
+                            windowSize = windowSize,
+                            modifier = Modifier.weight(1f),
+                            onItemTap = onStockPreviewTap,
+                        )
                     }
                 }
             }
@@ -144,10 +153,33 @@ fun MainScreen(
 }
 
 @Composable
-private fun StocksListContent(stocksPreview: List<StockPreview>) {
+private fun SearchString(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        value = text,
+        onValueChange = { onTextChange(it) },
+        modifier = modifier,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+        ),
+    )
+}
+
+@Composable
+private fun StocksPreviewListContent(
+    stocksPreview: List<StockPreview>,
+    windowSize: WindowSizeClass,
+    modifier: Modifier = Modifier,
+    onItemTap: (StockPreview) -> Unit = {},
+) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp)
+        modifier = modifier.animateContentSize(),
+        contentPadding = PaddingValues(top = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         items(
             count = stocksPreview.size,
@@ -155,10 +187,44 @@ private fun StocksListContent(stocksPreview: List<StockPreview>) {
                 stocksPreview[index].symbol.hashCode()
             }
         ) { index ->
-            stocksPreview[index].let { item ->
-                Text(item.symbol)
-            }
+            ItemContent(
+                ui = stocksPreview[index],
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                windowSize = windowSize,
+                onTap = onItemTap,
+            )
         }
     }
 }
 
+@Composable
+private fun ItemContent(
+    ui: StockPreview,
+    windowSize: WindowSizeClass,
+    modifier: Modifier = Modifier,
+    onTap: (StockPreview) -> Unit = {},
+) {
+    Column(
+        modifier = modifier
+            .heightIn(64.dp)
+            .clickable {
+                onTap(ui)
+            },
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = ui.symbol,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            style = getAdaptiveLabelByHeight(windowSize.heightSizeClass),
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = ui.longName,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            style = getAdaptiveBodyByHeight(windowSize.heightSizeClass),
+        )
+    }
+}
